e015b53a6b439574c98ee2f0d79b7f6adfb98f3e
Nándor Holozsnyák <holi60@gmail.com>
2016-10-02 13:04:32 +0200

    Plus test cases.

--- telefonszam-regexp/src/test/java/hu/unideb/inf/fordprog/telefonszam/AcceptedPhoneNumberTest.java
+++ telefonszam-regexp/src/test/java/hu/unideb/inf/fordprog/telefonszam/AcceptedPhoneNumberTest.java
@@ -10,18 +10,19 @@ import org.junit.runners.Parameterized.Parameters;
 @RunWith(Parameterized.class)
 public class AcceptedPhoneNumberTest {
 
-	@Parameters(name = "{index}: phone number: {0}")
-	public static Object[] data() {
-		return new Object[] { "+36-1-123123", "+36-52-123123", "+36301234567", "+36-30-123-45-67", "304290661",
-				"+36-52-333-333", "+36-30-123-12-12", "+36-30-123-1234", "+36-30-12-12-123", "+36-30-1234-123" };
-	}
+    @Parameters(name = "{index}: phone number: {0}")
+    public static Object[] data() {
+        return new Object[] { "+36-1-123123", "+36-52-123123", "+36301234567", "+36-30-123-45-67", "304290661",
+                "+36-52-333-333", "+36-30-123-12-12", "+36-30-123-1234", "+36-30-12-12-123", "+36-30-1234-123",
+                "+36-42-374-163" };
+    }
 
-	@Parameter
-	public String input;
+    @Parameter
+    public String input;
 
-	@Test
-	public void testAutomataAcceptInputs() {
-		Assert.assertTrue(PhoneNumberMatcher.match(input));
-	}
+    @Test
+    public void testAutomataAcceptInputs() {
+        Assert.assertTrue(PhoneNumberMatcher.match(input));
+    }
 
 }
