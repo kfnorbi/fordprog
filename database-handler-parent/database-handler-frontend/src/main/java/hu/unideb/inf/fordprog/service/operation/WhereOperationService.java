e2435ecdb7a69e18f93c4f08e2751702e7e9002b
Nándor Holozsnyák <holi60@gmail.com>
2016-12-19 18:13:58 +0100

    SQL refactor

--- database-handler-parent/database-handler-frontend/src/main/java/hu/unideb/inf/fordprog/service/operation/WhereOperationService.java
+++ database-handler-parent/database-handler-frontend/src/main/java/hu/unideb/inf/fordprog/service/operation/WhereOperationService.java
@@ -9,9 +9,12 @@ import org.slf4j.LoggerFactory;
 import hu.unideb.inf.fordprog.antlr4.DatabaseHandlerParser.ExpressionLabelContext;
 import hu.unideb.inf.fordprog.antlr4.DatabaseHandlerParser.Simple_expressionContext;
 import hu.unideb.inf.fordprog.antlr4.DatabaseHandlerParser.WhereClauseContext;
+import hu.unideb.inf.fordprog.error.ColumnTypeException;
 import hu.unideb.inf.fordprog.model.DatabaseData;
 import hu.unideb.inf.fordprog.model.DatabaseSelectRecord;
 import hu.unideb.inf.fordprog.model.DatabaseSelectResult;
+import hu.unideb.inf.fordprog.model.DatabaseTableColumnType;
+import hu.unideb.inf.fordprog.service.DatabaseHelper;
 
 /**
  * A WHERE feltételt kiszolgáló szolgáltatás.
@@ -19,16 +22,30 @@ import hu.unideb.inf.fordprog.model.DatabaseSelectResult;
  */
 public class WhereOperationService extends AbstractOperationService {
 
+    private static final String NOT_EQUALS_OPERATOR = "!=";
+
+    private static final String GREATER_THAN = ">";
+
+    private static final String GREATER_THAN_EQUALS = ">=";
+
+    private static final String LESS_THAN = "<";
+
+    private static final String LESS_THAN_EQUALS = "<=";
+
+    private static final String EQUALS_OPERATOR = "=";
     /**
      * Osztály loggere.
      */
     private static final Logger LOGGER = LoggerFactory.getLogger(WhereOperationService.class);
 
     /**
+     * Beérkező adatok szűrése kontextuson keresztül.
      *
      * @param ctx
+     *            a kontextus.
      * @param selectResult
-     * @return
+     *            a legelső lekérdezés eredménye.
+     * @return a szűrt lekérdezés eredménye.
      */
     public DatabaseSelectResult filterByContext(WhereClauseContext ctx, DatabaseSelectResult selectResult) {
         DatabaseSelectResult result;
@@ -57,41 +74,64 @@ public class WhereOperationService extends AbstractOperationService {
         String condition = simpleExpression.right.getText();
         String operator = simpleExpression.relation.getText();
         LOGGER.debug("{} {} {}", column, operator, condition);
-        List<DatabaseData> result = data.stream().filter(p -> p.getColumnName().equals(column))
-                .filter(p -> isFitSqlCondition(p.getValue(), condition, operator)).collect(Collectors.toList());
+        List<DatabaseData> result = data.stream().filter(p -> p.getColumnName().equals(column)).filter(
+                p -> isFitToType(p, condition, operator) && isFitSqlCondition(p.getValue(), condition, operator))
+                .collect(Collectors.toList());
         return !result.isEmpty();
+    }
+
+    private boolean isFitToType(DatabaseData p, String condition, String operator) {
+        DatabaseTableColumnType type = p.getType();
+        if (isConditionEligibleForType(condition, operator, type)) {
+            return true;
+        }
+        throw new ColumnTypeException();
+    }
+
+    private boolean isConditionEligibleForType(String condition, String operator, DatabaseTableColumnType type) {
+        return isVarcharTypeCondition(condition, operator, type) || isNumberTypeCondition(condition, type);
+    }
+
+    private boolean isNumberTypeCondition(String condition, DatabaseTableColumnType type) {
+        return isNumberTypeAndData(condition, type);
+    }
+
+    private boolean isVarcharTypeCondition(String condition, String operator, DatabaseTableColumnType type) {
+        return isVarcharTypeAndData(condition, type) && isOperatorEqualsOrNotEquals(operator);
+    }
+
+    private boolean isNumberTypeAndData(String condition, DatabaseTableColumnType type) {
+        return DatabaseTableColumnType.NUMBER.equals(type) && DatabaseHelper.isNumber(condition);
+    }
+
+    private boolean isVarcharTypeAndData(String condition, DatabaseTableColumnType type) {
+        return DatabaseTableColumnType.VARCHAR.equals(type) && DatabaseHelper.isVarchar(condition);
+    }
+
+    private boolean isOperatorEqualsOrNotEquals(String operator) {
+        return EQUALS_OPERATOR.equals(operator) || NOT_EQUALS_OPERATOR.equals(operator);
     }
 
     private boolean isFitSqlCondition(String value, String condition, String operator) {
         boolean result;
         LOGGER.debug("{} {} {}", value, operator, condition);
-        Double numberValue;
-        Double numberCondition;
         switch (operator) {
-        case "=":
+        case EQUALS_OPERATOR:
             result = isEquals(value, condition);
             break;
-        case ">":
-            numberValue = Double.valueOf(value);
-            numberCondition = Double.valueOf(condition);
-            result = numberValue > numberCondition;
+        case LESS_THAN:
+            result = isLessThan(value, condition);
             break;
-        case "<":
-            numberValue = Double.valueOf(value);
-            numberCondition = Double.valueOf(condition);
-            result = numberValue < numberCondition;
+        case GREATER_THAN:
+            result = isGreaterThan(value, condition);
             break;
-        case "<=":
-            numberValue = Double.valueOf(value);
-            numberCondition = Double.valueOf(condition);
-            result = numberValue <= numberCondition;
+        case LESS_THAN_EQUALS:
+            result = isLessThanEquals(value, condition);
             break;
-        case ">=":
-            numberValue = Double.valueOf(value);
-            numberCondition = Double.valueOf(condition);
-            result = numberValue >= numberCondition;
+        case GREATER_THAN_EQUALS:
+            result = isGreaterThanEquals(value, condition);
             break;
-        case "!=":
+        case NOT_EQUALS_OPERATOR:
             result = !isEquals(value, condition);
             break;
         default:
@@ -100,6 +140,31 @@ public class WhereOperationService extends AbstractOperationService {
         }
         LOGGER.debug("SQL filter result: {}", result);
         return result;
+
+    }
+
+    private boolean isLessThan(String value, String condition) {
+        Double numberValue = Double.valueOf(value);
+        Double numberCondition = Double.valueOf(condition);
+        return numberValue < numberCondition;
+    }
+
+    private boolean isGreaterThan(String value, String condition) {
+        Double numberValue = Double.valueOf(value);
+        Double numberCondition = Double.valueOf(condition);
+        return numberValue > numberCondition;
+    }
+
+    private boolean isLessThanEquals(String value, String condition) {
+        Double numberValue = Double.valueOf(value);
+        Double numberCondition = Double.valueOf(condition);
+        return numberValue <= numberCondition;
+    }
+
+    private boolean isGreaterThanEquals(String value, String condition) {
+        Double numberValue = Double.valueOf(value);
+        Double numberCondition = Double.valueOf(condition);
+        return numberValue >= numberCondition;
     }
 
     private boolean isEquals(String value, String condition) {
@@ -110,6 +175,9 @@ public class WhereOperationService extends AbstractOperationService {
             numberValue = Double.valueOf(value);
             numberCondition = Double.valueOf(condition);
             result = numberValue.equals(numberCondition);
+        } else if (DatabaseHelper.isVarchar(condition)) {
+            String cuttedCondition = DatabaseHelper.removeApostrophes(condition);
+            result = value.equals(cuttedCondition);
         } else {
             result = value.equals(condition);
         }
